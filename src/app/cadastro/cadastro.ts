ee74a9d7f80ba021bc346f52fa1b472cd2241b9a
lucsndr <lucasdsilvasouza@outlook.com>
2026-04-10 11:07:13 -0300

    Atualizações e APIs

--- src/app/cadastro/cadastro.ts
+++ src/app/cadastro/cadastro.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule, FormControl, FormGroup, Validators} from '@angular/forms';
+import { EnderecoService } from '../services/endereco.service';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule],
   templateUrl: './cadastro.html',
   styleUrl: './cadastro.css',
 })
 export class Cadastro {
+  private enderecoService = inject(EnderecoService);
+passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$/;
+form = new FormGroup ({
+  nome: new FormControl('',[Validators.required]),
+  email: new FormControl('',[Validators.required, Validators.email]),
+  senha: new FormControl('',[Validators.required, Validators.pattern(this.passwordRegex)]),
+  cep: new FormControl(''),
+  logradouro: new FormControl({ value: '', disabled: true })
+});
+
+onSubmit(){
+  if(this.form.valid)
+    console.log("Formulário válido", this.form.value);
+  else
+    console.log("Formulário inválido: ", this.form.value);
+}
+
 regraMinimoCaractere: Boolean = false;
 regraMaximoCaractere: Boolean = false;
 regraLetraMaiuscula: Boolean = false;
@@ -30,5 +49,14 @@ validarSenha(senha: string): void {
   this.regraLetraMaiuscula = rxMaiuscula.test(senha);
   this.regraCaractereEspecial = rxEspecial.test(senha);
   this.regraNumerosLetras = rxNumero.test(senha) && rxLetra.test(senha);
+}
+
+get buscarCep(){
+  const cep = this.form.get('cep')?.value ?? '';
+  this.enderecoService.getEndereco(cep).subscribe(endereco => {
+    this.form.get('logradouro')?.setValue(endereco.logradouro);
+  });
+
+  return true;
 }
 }
